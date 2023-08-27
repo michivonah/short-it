043e2d0a6fc3e0e0ece85dda4d39826918913498
michivonah <info@michivonah>
2023-08-27 21:06:06 +0200

    add example data for the other two tables

--- sql/sample_data.sql
+++ sql/sample_data.sql
@@ -3,3 +3,9 @@ INSERT INTO "url" ("slug", "destination", "description") VALUES
 	('demo', 'https://youtube.ch', 'YouTube'),
 	('michivonah', 'https://michivonah.ch', 'michivonah.ch'),
 	('bbzw', 'https://beruf.lu.ch/bbzw', 'Website BBZW');
+
+INSERT INTO "user" ("name", "mail", "token") VALUES
+	('admin', 'example@example.com', 'DixIGzHLoZGvo9m4gsXhPyoXViWRktJZ');
+
+INSERT INTO "analytics" ("fk_urlid") VALUES
+	((SELECT "urlid" FROM "url" WHERE "slug" = 'demo'));
