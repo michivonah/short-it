787c562bdcd06de59710f3f1a099085bb83146ce
michivonah <info@michivonah>
2023-08-27 20:58:52 +0200

    create ability to open link from database

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, jsonify, redirect
+from flask import Flask, request, jsonify, redirect, render_template
+import dbfunctions as db
+import re
 
 app = Flask(__name__)
 
@@ -8,8 +10,16 @@ def createNew(slug):
 
 @app.route("/<slug>")
 def shortUrl(slug):
-    destination = f"https://www.example.com/{slug}"
-    return redirect(destination, code=302)
+    defaultpage = "https://example.com"
+    if re.search("^[0-9a-zA-Z]*$", slug):
+        destination = db.executeQuery(f'SELECT "destination" FROM "url" WHERE "slug" = \'{str.lower(slug)}\';')
+        if destination:
+            destination = destination[0][0]
+        else:
+            destination = defaultpage
+        return redirect(destination, code=302)
+    else:
+        return render_template("error.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
