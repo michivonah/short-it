850922ceacc3fd7d5c9af3664c62e51e2f409c79
michivonah <info@michivonah>
2023-08-27 20:26:03 +0200

    create script with sample data for testing

---
+++ sql/sample_data.sql
@@ -0,0 +1,5 @@
+-- Add sample data to database
+INSERT INTO "url" ("slug", "destination", "description") VALUES
+	('demo', 'https://youtube.ch', 'YouTube'),
+	('michivonah', 'https://michivonah.ch', 'michivonah.ch'),
+	('bbzw', 'https://beruf.lu.ch/bbzw', 'Website BBZW');
